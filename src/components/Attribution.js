3f16cb43c3d2b1bb41687a98158623b9ad052d6b
Müller Csaba <csaba.muller@hotmail.com>
2023-07-03 23:00:47 +0200

    first commit

---
+++ src/components/Attribution.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Attribution() {
+  return (
+    <div>
+      <div className="attribution">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io/challenges/newsletter-signup-form-with-success-message-3FC1AZbNrv"
+          target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="https://www.frontendmentor.io/profile/Csabooo">Csaba Müller</a>
+        .
+      </div>
+    </div>
+  );
+}
+
+export default Attribution;
