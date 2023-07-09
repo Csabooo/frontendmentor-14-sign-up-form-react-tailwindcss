d588373b44c7b18600599bac9bc9678a6d5b3dae
Müller Csaba <csaba.muller@hotmail.com>
2023-07-09 22:56:55 +0200

    React js first mobile version added

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Subscribe() {
+  const [enteredWarning, setWarning] = useState("");
+  const [enteredText, setText] = useState("");
+  const [isValid, setValid] = useState(true);
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const validRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (validRegex.test(enteredText)) {
+      console.log(validRegex.test(enteredText));
+      setWarning(
+        <label className="text-green-300 text-right block">Valid</label>
+      );
+      setValid(true);
+      console.log(`isvalid state is ${isValid}`);
+    } else {
+      console.log(validRegex.test(enteredText));
+      setWarning(
+        <label className="text-tomato text-right block">
+          Valid email requiered
+        </label>
+      );
+      setValid(false);
+      console.log(`isvalid state is ${isValid}`);
+    }
+  };
+
+  const textHandler = (event) => {
+    const enteredmailtext = event.target.value;
+    setText(enteredmailtext);
+  };
+
+  const clearInput = (event) => {
+    event.target.value = "";
+    setValid(true);
+    setWarning("");
+  };
+
   return (
     <div>
-      <form className="flex flex-col">
-        <label>Email address</label>
+      <form className="flex flex-col" onSubmit={formSubmitHandler}>
+        <label>Email address {enteredWarning}</label>
+
         <input
-          className="inputField"
+          className={isValid ? "inputField" : "inputField error"}
           type="text"
           placeholder="email@company.com"
+          onChange={textHandler}
+          onFocus={clearInput}
         />
         <button className="button-bg" type="submit">
           Subscribe to monthly newsletter
