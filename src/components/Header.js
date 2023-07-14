bb86d150a31c7b6088d45cc396c41650dde6aff8
Müller Csaba <csaba.muller@hotmail.com>
2023-07-15 00:14:34 +0200

    form link updated in success component v2

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ function Header() {
     <div>
       <img
         className="object-cover w-96 bg-cover lg:hidden"
-        src="../images/illustration-sign-up-mobile.svg"
+        src="./images/illustration-sign-up-mobile.svg"
         alt="mobile-illustration"></img>
 
       <img
         className="hidden lg:block lg:h-full"
-        src="../images/illustration-sign-up-desktop.svg"
+        src="./images/illustration-sign-up-desktop.svg"
         alt="desktop-illustration"></img>
     </div>
   );
