9c5d4c4016225037eef063fb909a214911036141
Müller Csaba <csaba.muller@hotmail.com>
2023-07-30 21:33:35 +0200

    img files fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import React from "react";
+import sign_up_mobile from "../images/illustration-sign-up-mobile.svg";
+import sign_up_desktop from "../images/illustration-sign-up-desktop.svg";
 
 function Header() {
   return (
     <div>
       <img
         className="object-cover w-96 bg-cover lg:hidden"
-        src="./images/illustration-sign-up-mobile.svg"
+        src={sign_up_mobile}
         alt="mobile-illustration"></img>
 
       <img
         className="hidden lg:block lg:h-full"
-        src="./images/illustration-sign-up-desktop.svg"
+        src={sign_up_desktop}
         alt="desktop-illustration"></img>
     </div>
   );
